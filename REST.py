3ad9f2a9846aef5cfc9cc9638e4d8bc2a4782a4d
Kelly Danger <kellyanndanger@gmail.com>
2020-12-03 08:52:18 -0600

    starting to add db

--- REST.py
+++ REST.py
@@ -1,8 +1,24 @@
 # import flask
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 # set up application, referencing this file
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres:///test.db'
+# initialize the db
+db = SQLAlchemy(app)
+
+# https://www.youtube.com/watch?v=Z1RJmh_OqeA 
+# at 18:37
+# Need to figure out how to do this with postgres
+
+class Todo(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  content = db.Column(db.String(200), nullable=False)
+  date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+
 
 # creates route @ used for Flask
 @app.route('/')
